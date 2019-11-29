23fbdb7f3a6f188bd6392ecfedb7d22b1ab7c852
Esteban <efvilloldo@gmail.com>
2019-11-29 12:36:19 -0300

    Capitulo 07 - Exports - Modularizacion ACTUALIZADO

--- form_params.js
+++ form_params.js
@@ -10,10 +10,7 @@ var http = require("http"),
     if (req.url.indexOf("favicon.ico") > 0){ return; }
 
     fs.readFile("./index.html",function(err,html){
-        
-        
         var parametros = parser.parse(req);
-
        
         // Devuelve el html
         res.writeHead(200,{"Content-Type":"text/html"})
--- render_view.js
+++ render_view.js
@@ -2,7 +2,7 @@ function render(html, parametros) {
     var html_string = html.toString();
     // Expresion regular - El patron busca el valor entre las llaves
     var variables = html_string.match(/[^\{\}]+(?=\})/g);
-    var nombre = "nodejs...";
+    //var nombre = "nodejs...";
 
     // Reemplazamos las variables por los contenidos por el eval
     for (var i = variables.length - 1; i >= 0; i--) {
